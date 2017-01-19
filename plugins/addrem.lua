48721c84a016353fc675bfd6633fb911f041a434
mostafapoent <meno.lol54@gmail.com>
2017-01-19 18:27:21 +0300

    Update addrem.lua

--- plugins/addrem.lua
+++ plugins/addrem.lua
@@ -1,3 +1,5 @@
+--@deve_poent
+--@DEV_s_o
 local function addgroup(msg)
 local group = load_data('bot/group.json')
 local groupa = group[tostring(msg.chat_id)]
@@ -60,3 +62,5 @@ return {
   },
   run = run
 }
+--@deve_poent
+--@DEV_s_o
