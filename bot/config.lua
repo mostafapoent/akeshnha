2cf3eed6a093265f9d5ec13f3ecd27a9927ecce0
mostafapoent <meno.lol54@gmail.com>
2017-01-19 17:43:56 +0300

    Add files via upload

---
+++ bot/config.lua
@@ -0,0 +1,26 @@
+do local _ = {
+  about_text = "! Masters Bot 2.1v 🔰\n\nThe advanced administration bot based on Tg-Cli. 🌐\n\nIt was built on a platform TeleSeed after it has been modified.🔧🌐\n\nhttps://github.com/MastersDev\n\nProgrammer🔰\n@iDev1\n\nSpecial thanks to😋❤️\nTeleSeed Team\nMico \nMouamle\nOscar\n\nOur channels 😍👍🏼\n@MastersDev 🌚⚠️\n@OSCARBOTv2 🌚🔌\n@MouamleAPI 🌚🔩\n@Malvoo 🌚🔧\n \nMy YouTube Channel\nhttps://www.youtube.com/channel/UCKsJSbVGNGyVYvV5B2LrUkA",
+  enabled_plugins = {
+    "addrem",
+    "bc",
+    "lock",
+    "rank",
+    "msg_checks",
+    "help",
+    "id",
+  },
+  group  = {
+    data = "bot/group.json"
+  },
+  sudo_users = {
+    250191888,
+    257529394,
+    
+  },
+robot = {
+    230849872,
+    0
+  }
+}
+return _
+end
